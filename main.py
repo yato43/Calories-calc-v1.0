42a3baefe32db883931aaa580acee0fa3fc3e1aa
yato <akochenokvm@gmail.com>
2022-04-10 00:08:13 +0300

    complete first version

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-test = open('test.txt', 'a', encoding='utf-8')
+file_name = input('Введите название файла: ') + '.txt'
+test = open(file_name, 'a', encoding='utf-8')
 test_str = []
 
-def diet (kkal, p, f, c):
+
+def diet(kkal, p, f, c):
     while True:
-        print('Осталось: \n','калорий:',kkal,'Белков:', p,'Жиров:', f,'Углеводов:', c)
-        answer = input('Добваить еще продукт? ')
+        print('Осталось: \n', 'калорий:', kkal, 'Белков:', p, 'Жиров:', f, 'Углеводов:', c)
+        answer = input('Добавить еще продукт? ')
         if answer.lower() == 'да':
             product = input('Введите продукт: ')
             product_weight = int(input('Введите массу продукта: '))
@@ -12,22 +14,26 @@ def diet (kkal, p, f, c):
             product_proteins = float(input('Введите кол-во белков на 100 г.: '))
             product_fats = float(input('Введите кол-во жиров на 100 г.: '))
             product_carbons = float(input('Введите кол-во углеводов на 100 г.: '))
-            kkal -= int(product_kkal/100 * product_weight)
-            p -= int(product_proteins/100 * product_weight)
-            f -= int(product_fats/100 * product_weight)
-            c -= int(product_carbons/100 * product_weight)
-            new_record ='\n' + product + ' - ' + str(product_weight) + 'г.'
+            kkal -= int(product_kkal / 100 * product_weight)
+            p -= int(product_proteins / 100 * product_weight)
+            f -= int(product_fats / 100 * product_weight)
+            c -= int(product_carbons / 100 * product_weight)
+            new_record = '\n' + product + ' - ' + str(product_weight) + 'г.'
             test.write(new_record)
         else:
             break
+    total = int(kkal), int(p), int(f), int(c)
+    return total
+
+
 # antropometric data
 
-# height = int(input('Введите рост в см: '))
-# weight = int(input('Введите ваш вес в кг: '))
-# age = int(input('Введите возраст: '))
-height = 189
-weight = 142
-age = 45
+height = int(input('Введите рост в см: '))
+weight = int(input('Введите ваш вес в кг: '))
+age = int(input('Введите возраст: '))
+# height = 189
+# weight = 142
+# age = 45
 
 # calculating kkal deficit and PFC
 kkal_normal = (((9.99 * weight) + (6.25 * height) - (4.92 * age) - 161) * 0.8)
@@ -37,28 +43,41 @@ fats = int((kkal_normal * 0.3) / 9)
 carbohydrates = int((kkal_normal * 0.4) / 4)
 kkal_normal = int(kkal_normal)
 
-proteins_str = 'Норма белков: ' + str(kkal_normal) + ' г'
-fats_str = 'Норма Жиров: ' + str(kkal_normal) + ' г'
-carbohydrates_str = 'Норма Углеводов: ' + str(kkal_normal) + ' г'
+proteins_str = 'Норма белков: ' + str(proteins) + ' г'
+fats_str = 'Норма Жиров: ' + str(fats) + ' г'
+carbohydrates_str = 'Норма Углеводов: ' + str(carbohydrates) + ' г'
 kkal_normal_str = 'Норма калорий: ' + str(kkal_normal) + ' ккал'
-
 
 normal_list = [kkal_normal_str, proteins_str, carbohydrates_str, fats_str]
 normal_list = '\n'.join(normal_list)
 test.write(normal_list)
 
-test.write('\n\nЗавтрак.')
+test.write('\n\nЗавтрак.\n')
+print('\n\nЗавтрак. \n')
+total = diet(kkal_normal * 0.25, proteins * 0.25, fats * 0.25, carbohydrates * 0.25)
+(total_kkal, total_proteins, total_fats, total_carbons) = total
 
-test.write('\n\nланч')
+test.write('\n\nланч\n')
+print('\n\nланч\n')
+total = diet(kkal_normal * 0.15 + total_kkal, proteins * 0.15 + total_proteins, fats * 0.15 + total_fats,
+     carbohydrates * 0.15 + total_carbons)
+(total_kkal, total_proteins, total_fats, total_carbons) = total
 
-test.write('\n\nОбед.')
+test.write('\n\nОбед.\n')
+print('\n\nОбед.\n')
+total = diet(kkal_normal * 0.3 + total_kkal, proteins * 0.3 + total_proteins, fats * 0.3 + total_fats,
+     carbohydrates * 0.3 + total_carbons)
+(total_kkal, total_proteins, total_fats, total_carbons) = total
 
-test.write('\n\nПолдник')
+test.write('\n\nПолдник.\n')
+print('\n\nПолдник.\n')
+total = diet(kkal_normal * 0.15 + total_kkal, proteins * 0.15 + total_proteins, fats * 0.15 + total_fats,
+     carbohydrates * 0.15 + total_carbons)
+(total_kkal, total_proteins, total_fats, total_carbons) = total
 
-test.write('\n\nУжин')
-
-
-
-diet(int(kkal_normal), int(proteins), int(fats), int(carbohydrates))
+test.write('\n\nУжин.\n')
+print('\n\nУжин.\n')
+diet(kkal_normal * 0.15 + total_kkal, proteins * 0.15 + total_proteins, fats * 0.15 + total_fats,
+     carbohydrates * 0.15 + total_carbons)
 
 test.close()
